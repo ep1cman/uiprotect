b361f21e4665d19c9be93d9b231bf9db1d1ac2da
cyberpower678 <cybernet678@yahoo.com>
2024-06-11 18:55:06 -0400

    Fix code rabbit suggestions and lint errors
    Signed-off-by: cyberpower678 <cybernet678@yahoo.com>

--- src/uiprotect/api.py
+++ src/uiprotect/api.py
@@ -1486,11 +1486,11 @@ class ProtectApiClient(BaseApiClient):
         self,
         camera_id: str,
         filename: str,
-        output_file: Optional[Path] = None,
-        iterator_callback: Optional[IteratorCallback] = None,
-        progress_callback: Optional[ProgressCallback] = None,
+        output_file: Path | None = None,
+        iterator_callback: IteratorCallback | None = None,
+        progress_callback: ProgressCallback | None = None,
         chunk_size: int = 65536,
-    ) -> Optional[bytes]:
+    ) -> bytes:
         """
         Downloads a prepared MP4 video from a given camera.
 
@@ -1553,9 +1553,9 @@ class ProtectApiClient(BaseApiClient):
         end: datetime,
         channel_index: int = 0,
         validate_channel_id: bool = True,
-        fps: Optional[int] = None,
-        filename: Optional[str] = None,
-    ) -> Optional[Union[list[Any], dict[str, Any]]]:
+        fps: int | None = None,
+        filename: str | None =  None,
+    ) -> list[Any] | dict[str, Any] | None:
         """
         Prepares MP4 video from a given camera at a specific time.
 
@@ -1571,8 +1571,8 @@ class ProtectApiClient(BaseApiClient):
         You will receive a filename and an expiry time in seconds.
         """
         if validate_channel_id and self._bootstrap is not None:
-            camera = self._bootstrap.cameras[camera_id]
             try:
+                camera = self._bootstrap.cameras[camera_id]
                 camera.channels[channel_index]
             except IndexError as e:
                 raise BadRequest from e
@@ -1588,7 +1588,7 @@ class ProtectApiClient(BaseApiClient):
         else:
             params.update({"channel": channel_index})
 
-        if fps is not None:
+        if fps is not None and fps > 0:
             params["fps"] = fps
             params["type"] = "timelapse"
         else:
@@ -1616,12 +1616,12 @@ class ProtectApiClient(BaseApiClient):
         end: datetime,
         channel_index: int = 0,
         validate_channel_id: bool = True,
-        output_file: Optional[Path] = None,
-        iterator_callback: Optional[IteratorCallback] = None,
-        progress_callback: Optional[ProgressCallback] = None,
+        output_file: Path | None = None,
+        iterator_callback: IteratorCallback | None = None,
+        progress_callback: ProgressCallback | None = None,
         chunk_size: int = 65536,
-        fps: Optional[int] = None,
-    ) -> Optional[bytes]:
+        fps: [int] | None = None,
+    ) -> bytes:
         """
         Exports MP4 video from a given camera at a specific time.
 
@@ -1636,8 +1636,8 @@ class ProtectApiClient(BaseApiClient):
         (fps=20), and 600x (fps=40).
         """
         if validate_channel_id and self._bootstrap is not None:
-            camera = self._bootstrap.cameras[camera_id]
             try:
+                camera = self._bootstrap.cameras[camera_id]
                 camera.channels[channel_index]
             except IndexError as e:
                 raise BadRequest from e
@@ -1653,7 +1653,7 @@ class ProtectApiClient(BaseApiClient):
         else:
             params.update({"channel": channel_index})
 
-        if fps is not None:
+        if fps is not None and fps > 0:
             params["fps"] = fps
             params["type"] = "timelapse"
 
@@ -1704,13 +1704,13 @@ class ProtectApiClient(BaseApiClient):
         end: datetime,
         channel_index: int = 0,
         validate_channel_id: bool = True,
-        output_file: Optional[Path] = None,
-        iterator_callback: Optional[IteratorCallback] = None,
-        progress_callback: Optional[ProgressCallback] = None,
+        output_file: Path | None = None,
+        iterator_callback: IteratorCallback | None = None,
+        progress_callback: ProgressCallback | None = None,
         chunk_size: int = 65536,
-        fps: Optional[int] = None,
-        filename: Optional[str] = None,
-    ) -> Optional[bytes]:
+        fps: int | None,
+        filename: str | None,
+    ) -> bytes:
         """
         Deprecated: maintained for backwards compatibility.
 
